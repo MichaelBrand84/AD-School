da0bfc5fdb1611d96dfd8ed99da971f20c18a332
Michael Brand <michael.brand@ems-schiers.ch>
2023-04-21 06:32:34 +0200

    Korrekturen ClassificationTest

--- ClassificationTest.py
+++ ClassificationTest.py
@@ -30,35 +30,35 @@ ax.scatter(dataset[0:50,0], dataset[0:50,1], c = 'red', s = 20, alpha = 0.6)
 ax.scatter(dataset[50:100,0], dataset[50:100,1], c = 'green', marker = '^', s = 20, alpha = 0.6)
 ax.scatter(dataset[100:150,0], dataset[100:150,1], c = 'blue', marker = '*', s = 20, alpha = 0.6)
 ax.set_xlabel('Kelchblattlaenge (cm)')
-ax.set_xlabel('Kelchblattbreite (cm)')
+ax.set_ylabel('Kelchblattbreite (cm)')
 
 ax = fig.add_subplot(2,2,2)
 ax.scatter(dataset[0:50,2], dataset[0:50,3], c = 'red', s = 20, alpha = 0.6)
 ax.scatter(dataset[50:100,2], dataset[50:100,3], c = 'green', marker = '^', s = 20, alpha = 0.6)
 ax.scatter(dataset[100:150,2], dataset[100:150,3], c = 'blue', marker = '*', s = 20, alpha = 0.6)
 ax.set_xlabel('Kronblattlaenge (cm)')
-ax.set_xlabel('Kronblattbreite (cm)')
+ax.set_ylabel('Kronblattbreite (cm)')
 
 ax = fig.add_subplot(2,2,3)
 ax.scatter(dataset[0:50,0], dataset[0:50,2], c = 'red', s = 20, alpha = 0.6)
 ax.scatter(dataset[50:100,0], dataset[50:100,2], c = 'green', marker = '^', s = 20, alpha = 0.6)
 ax.scatter(dataset[100:150,0], dataset[100:150,2], c = 'blue', marker = '*', s = 20, alpha = 0.6)
 ax.set_xlabel('Kelchblattlaenge (cm)')
-ax.set_xlabel('Kronblattlaenge (cm)')
+ax.set_ylabel('Kronblattlaenge (cm)')
 
 ax = fig.add_subplot(2,2,4)
 ax.scatter(dataset[0:50,1], dataset[0:50,3], c = 'red', s = 20, alpha = 0.6)
 ax.scatter(dataset[50:100,1], dataset[50:100,3], c = 'green', marker = '^', s = 20, alpha = 0.6)
 ax.scatter(dataset[100:150,1], dataset[100:150,3], c = 'blue', marker = '*', s = 20, alpha = 0.6)
 ax.set_xlabel('Kelchblattbreite (cm)')
-ax.set_xlabel('Kronblattbreite (cm)')
+ax.set_ylabel('Kronblattbreite (cm)')
 
 plt.show()
 """
 
 
 # Testdaten extrahieren
-X = dataset[:, 1:4] # Messwerte
+X = dataset[:, 0:4] # Messwerte
 Y = dataset[:, 4]   # Art
 allData = np.arange(0, X.shape[0])
 testIndices = np.random.choice(X.shape[0], size = 30, replace = False)
@@ -69,3 +69,4 @@ YTrain = Y[trainIndices]
 XTest = X[testIndices, :]
 YTest = Y[testIndices]
 
+print(type(YTest[5]))
