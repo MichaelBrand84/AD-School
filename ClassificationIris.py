636aee462dd842de79347151cb0ba51ae9107525
Michael Brand <michael.brand@ems-schiers.ch>
2023-05-02 14:14:43 +0200

    mathaad.py nur das Nötigste, ClassificationIris

--- ClassificationIris.py
+++ ClassificationIris.py
@@ -3,15 +3,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from floataad import float2FloatAad, getValues, getGradient
+import mathaad
+
 # Daten einlesen
-# Label in 1 (Iris-setosa), bzw. 2 (Iris-versicolor), bzw. 3 (Iris-virginica) ändern
+# Label in 0 (Iris-setosa), bzw. 1 (Iris-versicolor), bzw. 2 (Iris-virginica) ändern
 fString = open('iris.data','r')
 fFloat  = open('iris.csv','w')
 
 for line in fString:
-    line = line.replace('Iris-setosa', '1')
-    line = line.replace('Iris-versicolor', '2')
-    line = line.replace('Iris-virginica', '3')
+    line = line.replace('Iris-setosa', '0')
+    line = line.replace('Iris-versicolor', '1')
+    line = line.replace('Iris-virginica', '2')
     fFloat.write(line)
 
 fString.close()
@@ -21,7 +24,7 @@ fFloat = open('iris.csv','r')
 dataset = np.loadtxt(fFloat, delimiter = ',')
 fFloat.close()
 
-
+'''
 # Daten plotten
 fig = plt.figure(1)
 
@@ -54,19 +57,59 @@ ax.set_xlabel('Kelchblattbreite (cm)')
 ax.set_ylabel('Kronblattbreite (cm)')
 
 plt.show()
+'''
 
 
-
-# Testdaten extrahieren
+# Daten in Trainings- und Testdaten aufteilen
 X = dataset[:, 0:4] # Messwerte
-Y = dataset[:, 4]   # Art
+Y = dataset[:, 4]   # Label
 allData = np.arange(0, X.shape[0])
 testIndices = np.random.choice(X.shape[0], size = 30, replace = False)
 trainIndices = np.delete(allData, testIndices)
 dataRecords = len(testIndices)
 XTrain = X[trainIndices, :]
-YTrain = Y[trainIndices]
+YTrain = np.array(Y[trainIndices], dtype = np.int32)
 XTest = X[testIndices, :]
 YTest = Y[testIndices]
 
-print(type(YTest[5]))
+# Matrix der Trainingsdaten mit Spalte bestehend aus 1 erweitern
+XTrain = np.c_[XTrain, np.ones(len(Y) - dataRecords)]
+
+
+
+# Gewichte initialisieren
+W = np.random.random(5 * 3)
+
+
+
+
+def loss(W, XTrain, YTrain):
+    def softmax(z):
+        return [mathaad.exp(x) / sum(mathaad.exp(z)) for x in z]
+    
+    N = len(YTrain) # Anzahl Trainingsdaten
+    
+    # Gewichte als FloatAad-Matrix
+    Wtemp = float2FloatAad(W)
+    Wtemp = np.reshape(Wtemp, [5, 3])
+    
+    
+    # Labels als one hot encoding
+    YOneHot = np.zeros([N, 3])
+    YOneHot[range(N), YTrain] = 1
+
+    Z = XTrain @ Wtemp
+    # Softmax auf jede Zeile anwenden
+    Yp = np.apply_along_axis(softmax, 1, Z)
+    
+    # Cross Entropy
+    D = [- YOneHot[i] @ mathaad.log(Yp[i]) for i in range(N) ]
+    S = sum(D) / N
+    LossValue = getValues(S)
+    LossGrad = getGradient(W, S)
+    return [LossValue, LossGrad]
+
+
+[Lval, Lgrad] = loss(W, XTrain, YTrain)
+print(Lval)
+print(Lgrad)  # Problem: alle Gradienten sind Null
